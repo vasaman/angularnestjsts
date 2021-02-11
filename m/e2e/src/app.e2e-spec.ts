a2c11dba6cc84d14618ee165aa9c31bd57c47fe9
admin <amanvas2601@gmail.com>
2021-02-11 09:25:56 +0530

    scaffolded by ng new m discared previous work

--- m/e2e/src/app.e2e-spec.ts
+++ m/e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', async () => {
     await page.navigateTo();
-    expect(await page.getTitleText()).toEqual('app2 app is running!');
+    expect(await page.getTitleText()).toEqual('m app is running!');
   });
 
   afterEach(async () => {
--- m/karma.conf.js
+++ m/karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/app2'),
+      dir: require('path').join(__dirname, './coverage/m'),
       subdir: '.',
       reporters: [
         { type: 'html' },
